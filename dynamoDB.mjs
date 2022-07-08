45a88844760291ede3feb4932a0f4ae4282b30fd
MahtHdz <maht_hernandez@outlook.com>
2022-07-08 17:37:38 -0500

    Files changed to mjs

--- dynamoDB.mjs
+++ dynamoDB.mjs
@@ -35,6 +35,82 @@ const createItem = async (ItemName, Item, TableName) => {
 
 /**
  * @description
+ * @param {string} ItemName
+ * @param {Object} Item
+ * @param {string} TableName
+ * @returns {Object}
+ */
+const updateItem = async (ItemName, Item, TableName) => {
+  let validationResponse = {}
+  const response = {
+    statusCode: 200,
+    isBase64Encoded: false,
+    body: JSON.stringify({ message: "Update item." }),
+    headers: { 'Content-Type': 'application/json;charset=UTF-8' }
+  }
+  // Get the id from the url params
+  const { id } = event?.pathParameters
+  // Transform the JSON string of body value to a javascript object
+  const req = JSON.parse(event.body)
+  const paramsArr = Object.keys(req)
+  if(req?.name){
+    /* try {
+      validationResponse = await itemExist( LAMBDA_NAME, req?.name )
+    } catch (error) {
+      response.statusCode = 500
+      response.body = JSON.stringify({
+        message: "Failed to validate item.",
+        error: error.message
+      })
+    } */
+  validationResponse.status = 200
+  } else validationResponse.status = true
+  if ( validationResponse?.status || validationResponse?.id === id ) {
+    const params = {
+      TableName,
+      Key: { id },
+      UpdateExpression: `SET ${paramsArr.map((key) => `#${key} = :${key}`).join(",")}`,
+      ExpressionAttributeNames: paramsArr.reduce((acc, key) => ({ ...acc, [`#${key}`]: key }), {}),
+      ExpressionAttributeValues: paramsArr.reduce((acc, key) => ({ ...acc, [`:${key}`]: req[key] }), {}),
+      ReturnValues: "UPDATED_NEW",
+    }
+    try {
+      await db.update(params).promise()
+      response.body = JSON.stringify({
+        message: "Item updated successfully.",
+        item: req,
+      })
+    } catch (error) {
+      console.error(error)
+      response.statusCode = 500
+      response.body = JSON.stringify({
+        message: "Failed to update item.",
+        error: error.message
+      })
+    }
+  } else {
+    response.statusCode = 403
+    response.body = JSON.stringify({
+      message: "Failed to update item.",
+      error: `Item "${req?.name}" already exists.`,
+    })
+  }
+  return response
+}
+
+/**
+ * @description
+ * @param {string} ItemName
+ * @param {Object} Item
+ * @param {string} TableName
+ * @returns {Object}
+ */
+const deleteItem = async (ItemName, Id, TableName) => {
+
+}
+
+/**
+ * @description
  * @param {boolean} Sorting
  * @param {string} ItemName
  * @param {string} BusinessId
@@ -114,7 +190,9 @@ const getItemByName = async(ItemName, Name, BusinessId, TableName) => {
 const dynamoDB = {
   createItem,
   getItemByName,
-  getItemOrderedByName
+  getItemOrderedByName,
+  updateItem,
+  deleteItem
 }
 
 export default dynamoDB
